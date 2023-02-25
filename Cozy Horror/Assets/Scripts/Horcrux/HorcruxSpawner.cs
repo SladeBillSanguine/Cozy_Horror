e942e0a218cf71a6f779b44fcf6a7a1baf14f202
AroNixBox <nixonbusinessmail@gmail.com>
2023-02-25 16:27:22 +0100

    ...

--- Cozy Horror/Assets/Scripts/Horcrux/HorcruxSpawner.cs
+++ Cozy Horror/Assets/Scripts/Horcrux/HorcruxSpawner.cs
@@ -6,23 +6,34 @@ public class HorcruxSpawner : MonoBehaviour
 {
     public GameObject[] newHorcrux;
     [SerializeField] HorcruxPatrol _neuePosition;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnAtStart());
+    }
     public void SpawnNewHorcrux()
     {
         StartCoroutine(SpawnAfterTime());
     }
 
+    IEnumerator SpawnAtStart()
+    {
+        yield return new WaitForSeconds(30);
+        StartCoroutine(SpawnAfterTime());
+    }
     IEnumerator SpawnAfterTime()
     {
         transform.position = _neuePosition.GetTargetPosition();
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(30);
         GameObject nh = Instantiate(newHorcrux[Random.Range(0, newHorcrux.Length)], this.transform) as GameObject;
         nh.transform.localPosition = new Vector3(0, 0, 0);
-        StartCoroutine(DestroyAfterTime());
+        StartCoroutine(DestroyAfterTime(nh));
     }
-    IEnumerator DestroyAfterTime()
+    IEnumerator DestroyAfterTime(GameObject nh)
     {
         //Add Timer 60 seconds, to find the mimic, here
         yield return new WaitForSeconds(60);
-        Destroy(this.gameObject);
+        Destroy(nh);
+        StartCoroutine(SpawnAfterTime());
     }
 }
